de4d494409149484121bd593cd68339547e4326d
charlieg <greenbacker@gmail.com>
2014-09-07 00:58:26 -0400

    abstracted NER-to-CLAVIN format conversion into a separate static method for ease of use by folks who might be running Stanford NER separate from CLAVIN and just want to pass the output

--- src/main/java/com/bericotech/clavin/nerd/StanfordExtractor.java
+++ src/main/java/com/bericotech/clavin/nerd/StanfordExtractor.java
@@ -1,3 +1,33 @@
+/*#####################################################################
+ *
+ * CLAVIN-NERD
+ * -----------
+ *
+ * Copyright (C) 2012-2013 Berico Technologies
+ * http://clavin.bericotechnologies.com
+ *
+ * ====================================================================
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * ====================================================================
+ *
+ * StanfordExtractor.java
+ *
+ *###################################################################*/
+
 package com.bericotech.clavin.nerd;
 
 import com.bericotech.clavin.extractor.LocationExtractor;
@@ -12,36 +42,6 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
-
-/*#####################################################################
- * 
- * CLAVIN-NERD
- * -----------
- * 
- * Copyright (C) 2012-2013 Berico Technologies
- * http://clavin.bericotechnologies.com
- * 
- * ====================================================================
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- * 
- * ====================================================================
- * 
- * StanfordExtractor.java
- * 
- *###################################################################*/
 
 /**
  * Extracts location names from unstructured text documents using a
@@ -89,31 +89,39 @@ public class StanfordExtractor implements LocationExtractor {
      * Get extracted locations from a plain-text body.
      * 
      * @param text      Text content to perform extraction on.
-     * @return          List of Location Occurrences.
+     * @return          List of extracted Location Occurrences.
      */
     public List<LocationOccurrence> extractLocationNames(String text) {
         if (text == null)
             throw new IllegalArgumentException("text input to extractLocationNames should not be null");
 
-        List<LocationOccurrence> extractedLocations = new ArrayList<LocationOccurrence>();
-
         // extract entities as <Entity Type, Start Index, Stop Index>
-        List<Triple<String, Integer, Integer>> extractedEntities = 
-                namedEntityRecognizer.classifyToCharacterOffsets(text);
+        return convertNERtoCLAVIN(namedEntityRecognizer.classifyToCharacterOffsets(text), text);
+    }
 
-        if (extractedEntities != null) {
+    /**
+     * Converts output from Stanford NER to input required by CLAVIN resolver.
+     *
+     * @param entities  List<Triple<String, Integer, Integer>> from Stanford NER
+     * @param text      text content processed by Stanford NER + CLAVIN resolver
+     * @return          List<LocationOccurrence> used by CLAVIN resolver
+     */
+    public static List<LocationOccurrence> convertNERtoCLAVIN
+            (List<Triple<String, Integer, Integer>> entities, String text) {
+
+        List<LocationOccurrence> locations = new ArrayList<LocationOccurrence>();
+
+        if (entities != null) {
             // iterate over each entity Triple
-            for (Triple<String, Integer, Integer> extractedEntity : extractedEntities) {
+            for (Triple<String, Integer, Integer> entity : entities) {
                 // check if the entity is a "Location"
-                if (extractedEntity.first.equalsIgnoreCase("LOCATION")) {
+                if (entity.first.equalsIgnoreCase("LOCATION")) {
                     // build a LocationOccurrence object
-                    extractedLocations.add(new LocationOccurrence(
-                            text.substring(extractedEntity.second(), extractedEntity.third()),
-                            extractedEntity.second()));
+                    locations.add(new LocationOccurrence(text.substring(entity.second, entity.third), entity.second));
                 }
             }
         }
 
-        return extractedLocations;
+        return locations;
     }
 }
